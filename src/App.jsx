516fdd7eb1fabdfdf8620ad3a3db4eb773f97e46
Sergei Ratnikov <sergei.ratnikoff@yandex.ru>
2024-12-13 08:59:33 +0300

    task 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [todoItems, setTodoItems] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+
+  useEffect(() => {
+    setisLoading(true);
+
+    setTimeout(() => {
+      fetch('https://dummyjson.com/todos/random/10')
+        .then((requestData) => requestData.json())
+        .then((res) => {
+          setTodoItems(res);
+        });
+      setisLoading(false);
+    }, 2000);
+  }, []);
+
   return (
     <div className="app">
-      <p>Задание 1 в ветке - task1</p>
-      <p>Задание 2 в ветке - task2</p>
+      <div className="todo-list">
+        <h1>Simple ToDo List</h1>
+        <p color="red">{'(Data requested from DummyJSON.com)'}</p>
+        {isLoading ? (
+          'Requesting data, wait a second...'
+        ) : (
+          <ul>
+            {todoItems.map((todo) => (
+              <li className="todo-item" key={todo.id}>
+                <input type="checkbox" />
+                {todo.todo}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
   );
 }
