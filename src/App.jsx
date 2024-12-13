d87b0794cdc9667414161d253f50328af9731be7
Sergei Ratnikov <sergei.ratnikoff@yandex.ru>
2024-12-13 08:57:20 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <p>Задание 1 в ветке - task1</p>
+      <p>Задание 2 в ветке - task2</p>
+    </div>
+  );
+}
+
+export default App;
