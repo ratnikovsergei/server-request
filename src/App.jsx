c57ae21c3ba03ff5b649362e9325417ba35b6063
Sergei Ratnikov <sergei.ratnikoff@yandex.ru>
2024-12-31 14:30:38 +0300

    добавлена маршрутизация

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div className="app">
       <p>Задание 1 в ветке - task1</p>
       <p>Задание 2 в ветке - task2</p>
+      <p>Задание с маршрутизацией в ветке - routing</p>
     </div>
   );
 }
